5d10ea6728a1e317f76955c35e2f9157559d1078
RafaelKleinH <79064265+RafaelKleinH@users.noreply.github.com>
2021-11-25 20:51:44 -0300

    Adds
    Fiz no teclado do login e registro adicionando scrollView e adicionando a tela main com bottom navigation

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,99 +1,156 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pdmapp/components/textField.dart';
 
 class registerView extends StatelessWidget {
   final usernameTextController = TextEditingController();
   final passwordTextController = TextEditingController();
+  final confirmPasswordTextController = TextEditingController();
+  String? alertText;
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      title: 'Cupertino App',
-      home: CupertinoPageScaffold(
-        backgroundColor: CupertinoColors.darkBackgroundGray,
-        navigationBar: CupertinoNavigationBar(
-          backgroundColor: CupertinoColors.systemGrey.withOpacity(0),
-          middle: const Text(
-            '',
-          ),
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.darkBackgroundGray,
+      navigationBar: CupertinoNavigationBar(
+        leading: CupertinoNavigationBarBackButton(
+          onPressed: () => Navigator.pop(context, false),
+          color: CupertinoColors.white,
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 32, right: 32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Nome",
-                style: TextStyle(
-                  color: CupertinoColors.white.withAlpha(215),
-                  fontSize: 18.0,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Container(
-                  height: 56.0,
-                  child: bbtextfield(
-                    false,
-                    "Usuario",
-                    usernameTextController,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 24.0),
-                child: Text(
-                  "Senha",
+        backgroundColor: CupertinoColors.systemGrey.withOpacity(0),
+        middle: const Text(
+          '',
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Nome",
                   style: TextStyle(
                     color: CupertinoColors.white.withAlpha(215),
                     fontSize: 18.0,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Container(
-                  height: 56.0,
-                  child: bbtextfield(
-                    true,
-                    "Senha",
-                    passwordTextController,
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Container(
+                    height: 56.0,
+                    child: bbtextfield(
+                      false,
+                      "Usuario",
+                      usernameTextController,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 32.0),
-                child: Column(
-                  children: [
-                    CupertinoButton(
-                      child: Center(
-                        child: Text("Entrar"),
-                      ),
-                      color: CupertinoColors.white.withAlpha(220),
-                      onPressed: null,
+                Padding(
+                  padding: const EdgeInsets.only(top: 14.0),
+                  child: Text(
+                    "Senha",
+                    style: TextStyle(
+                      color: CupertinoColors.white.withAlpha(215),
+                      fontSize: 18.0,
                     ),
-                    OutlinedButton(
-                      child: Center(
-                        child: Text(
-                          "Registrar",
-                          style: TextStyle(
-                            color: CupertinoColors.white,
-                            fontSize: 16,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                      onPressed: null,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Container(
+                    height: 56.0,
+                    child: bbtextfield(
+                      true,
+                      "Senha",
+                      passwordTextController,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 14.0),
+                  child: Text(
+                    "Confirmar Senha",
+                    style: TextStyle(
+                      color: CupertinoColors.white.withAlpha(215),
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Container(
+                    height: 56.0,
+                    child: bbtextfield(
+                      true,
+                      "Confirmar Senha",
+                      confirmPasswordTextController,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 32.0),
+                  child: Column(
+                    children: [
+                      CupertinoButton(
+                        child: Center(child: Text("Registrar")),
+                        color: CupertinoColors.white.withAlpha(8),
+                        onPressed: () {
+                          bool isTextCorrect = isTextCorrectNotNull();
+                          bool isPasswordEqual = isPasswordsEquals();
+
+                          alertText = isTextCorrect
+                              ? null
+                              : "Todos campos devem estar preenchidos";
+                          debugPrint(alertText.toString());
+                          if (alertText == null) {
+                            alertText = isPasswordEqual
+                                ? null
+                                : "Senhas devem ser iguais.";
+                          }
+                          if (alertText == null) {
+                            //TO DO POST...
+                          } else {
+                            showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  CupertinoAlertDialog(
+                                title: Text("Opss!"),
+                                content: Text(alertText!),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: Text("OK"),
+                                    onPressed: () =>
+                                        Navigator.pop(context, "OK"),
+                                  )
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  bool isTextCorrectNotNull() {
+    return usernameTextController.text.isNotEmpty &&
+        passwordTextController.text.isNotEmpty &&
+        confirmPasswordTextController.text.isNotEmpty;
+  }
+
+  bool isPasswordsEquals() {
+    return passwordTextController.text == confirmPasswordTextController.text;
   }
 }
